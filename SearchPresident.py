8116ee27e0c9ff14e2e513c21150f61b831fb37a
MainTankJerry <qiuz@rose-hulman.edu>
2017-04-30 11:30:35 -0400

    finish skip-bigram

--- SearchPresident.py
+++ SearchPresident.py
@@ -5,29 +5,20 @@ import sys
 class SearchPresident:
 
 	def __init__(self):
-
-
-	def output():
 		pass
 
-
-
 	def start():
-		x = input ("Enter the query you want to search. Enter q to quit.")
-		words = x.split()
-		wordCount = len(words)
-		#skip-bigrams
-		if wordCount == 0:
-			pass
-		elif wordCount == 1:
-			pass
-		elif wordCount == 2:
-			pass
-		else: 
-			for i in range(wordCount - 1):
-				if 
+		
+		x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
+		
 
+		# Add stuff here if we want to handle sentence of length one.
 
+		bigrams = [b for b in zip(x.split(" ")[:-1], x.split(" ")[1:])]
+		skipgrams = [b for b in zip(x.split(" ")[:-2], x.split(" ")[2:])]
+		skipbigrams = bigrams + skipgrams
+		
+		print(skipbigrams)
 
 
 
