e899b6d21491b083c87dc309688e238658762ce9
MainTankJerry <qiuz@rose-hulman.edu>
2017-04-30 11:13:16 -0400

    add a little

--- SearchPresident.py
+++ SearchPresident.py
@@ -7,19 +7,27 @@ class SearchPresident:
 	def __init__(self):
 
 
-	def input():
-		pass
-
-
-
-
 	def output():
 		pass
 
 
 
-	def main():
-		pass
+	def start():
+		x = input ("Enter the query you want to search. Enter q to quit.")
+		words = x.split()
+		wordCount = len(words)
+		#skip-bigrams
+		if wordCount == 0:
+			pass
+		elif wordCount == 1:
+			pass
+		elif wordCount == 2:
+			pass
+		else: 
+			for i in range(wordCount - 1):
+				if 
+
+
 
 
 
@@ -28,4 +36,4 @@ class SearchPresident:
 
 
 	if __name__ == '__main__':
-  		main()
+  		start()
