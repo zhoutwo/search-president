c2294e7f35e09c37214b2746127b73b46ac05877
MainTankJerry <qiuz@rose-hulman.edu>
2017-04-30 11:46:53 -0400

    Fixed some set up problem

--- SearchPresident.py
+++ SearchPresident.py
@@ -1,30 +1,19 @@
 import math
 import sys
 
+def skip_bigram_helper(x):
+	# Add stuff here if we want to handle sentence of length one.
 
-class SearchPresident:
+	bigrams = [b for b in zip(x.split(" ")[:-1], x.split(" ")[1:])]
+	skipgrams = [b for b in zip(x.split(" ")[:-2], x.split(" ")[2:])]
+	skipbigrams = bigrams + skipgrams
+	return skipbigrams
 
-	def __init__(self):
-		pass
+def start():
+	x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
+	
+	skipbigrams = skip_bigram_helper(x)
+	print(skipbigrams)
 
-	def start():
-		
-		x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
-		
-
-		# Add stuff here if we want to handle sentence of length one.
-
-		bigrams = [b for b in zip(x.split(" ")[:-1], x.split(" ")[1:])]
-		skipgrams = [b for b in zip(x.split(" ")[:-2], x.split(" ")[2:])]
-		skipbigrams = bigrams + skipgrams
-		
-		print(skipbigrams)
-
-
-
-
-
-
-
-	if __name__ == '__main__':
-  		start()
+if __name__ == '__main__':
+	start()
