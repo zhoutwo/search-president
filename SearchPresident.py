26f9a782dc2039f9a6f484fd794c43b09aa7115b
MainTankJerry <qiuz@rose-hulman.edu>
2017-04-30 10:50:43 -0400

    class frame

---
+++ SearchPresident.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+
+class SearchPresident:
+
+	def __init__(self):
+
+
+	def input():
+		pass
+
+
+
+
+	def output():
+		pass
+
+
+
+	def main():
+		pass
+
+
+
+
+
+
+
+	if __name__ == '__main__':
+  		main()
