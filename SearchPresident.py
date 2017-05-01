e58965a0396720598516ebb2b79247c9c9b2457f
MainTankJerry <qiuz@rose-hulman.edu>
2017-05-01 19:27:01 -0400

    Nothing changed

--- SearchPresident.py
+++ SearchPresident.py
@@ -1,30 +1,31 @@
 import math
 import sys
 
+def skip_bigram_helper(x):
+	# Add stuff here if we want to handle sentence of length one.
 
-class SearchPresident:
+	bigrams = [b for b in zip(x.split(" ")[:-1], x.split(" ")[1:])]
+	skipgrams = [b for b in zip(x.split(" ")[:-2], x.split(" ")[2:])]
+	skipbigrams = bigrams + skipgrams
+	return skipbigrams
 
-	def __init__(self):
-		pass
+def start():
+	x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
+	
+	skipbigrams = skip_bigram_helper(x)
+	#print(skipbigrams)
 
-	def start():
-		
-		x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
-		
-
-		# Add stuff here if we want to handle sentence of length one.
-
-		bigrams = [b for b in zip(x.split(" ")[:-1], x.split(" ")[1:])]
-		skipgrams = [b for b in zip(x.split(" ")[:-2], x.split(" ")[2:])]
-		skipbigrams = bigrams + skipgrams
-		
-		print(skipbigrams)
+	#reading file 
 
 
 
 
+	bigrams
+	
+	# N = number of documents
+	# NDocuments = number of words occuring in each documents
+	# 
+	for ()
 
-
-
-	if __name__ == '__main__':
-  		start()
+if __name__ == '__main__':
+	start()
