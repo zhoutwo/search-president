59de6a410318511c360f3d5b17d78b4a38fa0908
MainTankJerry <qiuz@rose-hulman.edu>
2017-05-02 11:13:39 -0400

    Allow query to be a single word now

--- SearchPresident.py
+++ SearchPresident.py
@@ -22,60 +22,111 @@ def IDF(N, nqi):
 def cmpT(a, b):
 	return sorted(a) == sorted(b)
 
-def start():
-  x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
-      
-  skipbigrams = skip_bigram_helper(x)
-  print(skipbigrams)
-
+def BM25Scoring(x):
+  x = x.strip(',.\n"').lower()
+  
   # reading file 
   documentDict = convertAll('Presidents')
-  documents = dict()
-  for filename in documentDict:
-    documents[filename] = skip_bigram_helper(documentDict[filename])
     
   # N = number of documents
-  N = len(documents)
-  # count = number of words occuring in each documents
-  # D(filename, bigram) = number of time this bigram appears in filename
-  Davs = dict()
-  D = dict()
+  N = len(documentDict)
+  
   total_length = 0
   lengths = dict()
-  
   for filename in documentDict:
     lengths[filename] = len(documentDict[filename])     
     total_length += lengths[filename]
     
-    avg_length = total_length / N
+  avg_length = total_length / N
     
-  for filename in documents:
+  Davs = dict()
+  for filename in documentDict:
     Davs[filename] = 1.0 * lengths[filename] / avg_length
-    
-  n = dict()
-    
-  for skipbigram in skipbigrams:
-    n[skipbigram] = 0
-    for filename in documents:
-      if skipbigram in documents[filename]:
-        n[skipbigram] += 1
-          
+
+  # n counts for the number of documents that this word appears in.
+  n = 0
+  for filename in documentDict:
+    if x in documentDict[filename].strip(',.\n"').lower():
+      n[skipbigram] += 1
+   
+  # D = number of time this bigram appears in a file      
   score = dict()
   for filename in documentDict:
-    score[filename] = 0
-    for skipbigram in skipbigrams:
-      D[skipbigram] = 0
-      for documentBigram in documents[filename]:
-        if documentBigram == skipbigram:
-          D[skipbigram] += 1
-    for skipbigram in skipbigrams:
-      score[filename] += IDF(N, n[skipbigram]) * D[skipbigram] * (k + 1) / (D[skipbigram] + k * (1 - b + b * Davs[filename]))
+    D = 0
+    document = documentDict[filename].strip(',.\n"').lower()
+    for word in document:
+      if word == x:
+        D += 1
+    score[filename] = IDF(N, n) * D * (k + 1) / (D + k * (1 - b + b * Davs[filename]))
 
   scoreTuples = []
   for filename in score:
     scoreTuples.append((score[filename], filename))
   scoreTuples.sort(reverse=True)
   print scoreTuples
+
+
+def start():
+  
+  x = raw_input ("Enter the query you want to search. Enter q to quit.\n")
+  if len(x) == 0:
+    print "Input invalid"
+    exit()
+  elif len(x) == 1:
+    BM25Scoring(x)
+  else:
+
+    skipbigrams = skip_bigram_helper(x)
+    print(skipbigrams)
+
+    # reading file 
+    documentDict = convertAll('Presidents')
+    documents = dict()
+    for filename in documentDict:
+      documents[filename] = skip_bigram_helper(documentDict[filename])
+      
+    # N = number of documents
+    N = len(documents)
+    # count = number of words occuring in each documents
+    # D(filename, bigram) = number of time this bigram appears in filename
+    Davs = dict()
+    D = dict()
+    total_length = 0
+    lengths = dict()
+
+    for filename in documentDict:
+      lengths[filename] = len(documentDict[filename])     
+      total_length += lengths[filename]
+      
+    avg_length = total_length / N
+      
+    for filename in documents:
+      Davs[filename] = 1.0 * lengths[filename] / avg_length
+      
+    n = dict()
+      
+    for skipbigram in skipbigrams:
+      n[skipbigram] = 0
+      for filename in documents:
+        if skipbigram in documents[filename]:
+          n[skipbigram] += 1
+            
+    score = dict()
+    for filename in documentDict:
+      score[filename] = 0
+      for skipbigram in skipbigrams:
+        D[skipbigram] = 0
+        for documentBigram in documents[filename]:
+          if documentBigram == skipbigram:
+            D[skipbigram] += 1
+      for skipbigram in skipbigrams:
+        score[filename] += IDF(N, n[skipbigram]) * D[skipbigram] * (k + 1) / (D[skipbigram] + k * (1 - b + b * Davs[filename]))
+
+    scoreTuples = []
+    for filename in score:
+      scoreTuples.append((score[filename], filename))
+    scoreTuples.sort(reverse=True)
+    print scoreTuples
 	
 if __name__ == '__main__':
   start()
